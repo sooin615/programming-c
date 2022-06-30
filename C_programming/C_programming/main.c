8012749fe205655692527c8450b0089c1e3c29cf
agiss615 <sooin615@naver.com>
2022-06-30 10:16:59 +0900

    🎉 :: 첫 커밋!

---
+++ C_programming/C_programming/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  C_programming
+//
+//  Created by Agiss on 2022/06/29.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    char a;
+    scanf("%c", &a);
+    printf("%c", a);
+    return 0;
+}
